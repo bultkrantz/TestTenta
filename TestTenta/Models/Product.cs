6c6f4b861997153a89fd71c6c9d2f8de8490d52e
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-09-07 09:55:14 +0200

    Added validation attributes to Productmodel

--- TestTenta/Models/Product.cs
+++ TestTenta/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace TestTenta.Models
 {
     public class Product
     {
+        [Required]
         public string Name { get; set; }
+        [Range(5, 1000)]
         public decimal Price { get; set; }
     }
 }
